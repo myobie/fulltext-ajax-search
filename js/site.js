900d989a3a200e6e9d59fce06bf5a0c13df2d18f
myobie <nathan@myobie.com>
2011-03-30 11:01:06 -0400

    Refactor the fetching of the results into it's own function.

--- js/site.js
+++ js/site.js
@@ -1,9 +1,12 @@
-$("#search").submit(function(event) {
-  event.preventDefault(); // prevent the form from actually submitting
 
+function get_results() {
   // do the post ourselves in javascript
-  $.post("/templates/search-results.php", $(this).serialize(), function(data, status, request) {
+  $.post("/templates/search-results.php", $("#search").serialize(), function(data, status, request) {
     $("#results").html(data);
   });
+}
 
+$("#search").submit(function(event) {
+  event.preventDefault(); // prevent the form from actually submitting
+  get_results();
 });
