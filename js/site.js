f0af674ca57ccb4cc8abcc2c673a52c1e513736e
myobie <nathan@myobie.com>
2011-03-30 11:03:38 -0400

    Fetch results after they stop typing.

--- js/site.js
+++ js/site.js
@@ -10,3 +10,11 @@ $("#search").submit(function(event) {
   event.preventDefault(); // prevent the form from actually submitting
   get_results();
 });
+
+// setup to get results after they stop typing
+var delay_timeout_id = 0;
+
+$("#q").keyup(function(event) { // for every key they hit
+  clearTimeout(delay_timeout_id); // stop any currently running timers
+  delay_timeout_id = setTimeout(get_results, 300); // and setup to get results really soon from now
+});
